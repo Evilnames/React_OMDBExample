c39d6e7763a13d8ca8ae6bf79a5330efbb6095fe
Alex Kremer <Askremer@gmail.com>
2016-06-08 22:12:08 -0700

    Added detail panel.  Split LI's into their own class.

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,7 +1,8 @@
 var MovieContainer = React.createClass({
 	getInitialState : function(){
 		return{
-			Search : []
+			Search : [],
+			SelectedMovie : {}
 		}
 	},
 	handleMovieSearch : function(e){
@@ -15,13 +16,34 @@ var MovieContainer = React.createClass({
 					Search : data.Search
 				})
 			}
-		}.bind(this))
+		}.bind(this));
+	},
+	handleSelectedMovie : function(imdbID){
+		$.ajax({
+			url : 'http://www.omdbapi.com/?i='+ imdbID +'&y=&plot=short&r=json',
+			method : 'get',
+			crossDomain:true
+		}).done(function(data){
+			console.log(data)
+			if(this.isMounted()){
+				this.setState({
+					SelectedMovie : data
+				})
+			}
+		}.bind(this));
 	},
 	render : function(){
 		return (
 			<div>
-				<input type="text" id="searchForMovie" onChange={this.handleMovieSearch} />
-				<MovieList results={this.state.Search} />
+				<div class="movieSearchBar">
+					<input type="text" id="searchForMovie" onChange={this.handleMovieSearch} />
+				</div>
+				<div className="movieListArea">
+					<MovieList results={this.state.Search} drillDownResult={this.handleSelectedMovie} />
+				</div>
+				<div className="selectedMovieArea">
+					<SelectedMovie selectedMovie={this.state.SelectedMovie} />
+				</div>
 			</div>
 		)
 	}
@@ -35,28 +57,54 @@ var MovieList = React.createClass({
 		}
 	},
 	render : function(){
+		var _this = this;
 		var movies = this.props.results.map(function(movie){
-			var moviePoster = (movie.Poster !== 'N/A') ? <img src={movie.Poster} /> : ''; 
 			return(
-				<li id={movie.imdbId}>
-					{moviePoster}
-					 <br/> {movie.Title} - ({movie.Year}) 
-					
-				</li>
+				<MoviePoster movie={movie} clickAction = {_this.props.drillDownResult} />
 			)
-			;
 		});
 
 		return (
 			<div>
-				<h3>Movies Found</h3>
-				<ul>
+				<ul className="movieSearchResultHolder">
 					{movies}
 				</ul>
 			</div>
 		)
 	}
-})
+});
+
+var MoviePoster = React.createClass({
+	handleOnClick : function(e){
+		 this.props.clickAction(this.props.movie.imdbID);
+	},
+	render : function(){
+		var moviePoster = (this.props.movie.Poster !== 'N/A') ? <img className="MoviePoster"  src={this.props.movie.Poster} /> : ''; 
+		return (
+			<li onClick={this.handleOnClick}>
+				{moviePoster}
+				 <br/> {this.props.movie.Title} - ({this.props.movie.Year}) 
+			</li>
+		)
+	}
+});
+
+var SelectedMovie = React.createClass({
+	render : function(){
+		var moviePoster = (this.props.selectedMovie.Poster !== 'N/A') ? <img  src={this.props.selectedMovie.Poster} /> : ''; 
+		
+		return (
+			<div>
+				Detail
+				<h1>{this.props.selectedMovie.Title}</h1>
+				{moviePoster}
+				<h4>Awards</h4>
+				<p>{this.props.selectedMovie.Awards}</p>
+
+			</div>
+		)
+	}
+});
 
 //Renders the dom.
 ReactDOM.render(
